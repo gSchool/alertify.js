d09235f7c53322f5655506e1c64ee8f84828c4fb
labutineg <labutineg@avilex.ru>
2017-02-14 11:07:28 +0300

    Fix broken typings to modern and working, add typings to packages for automated using typings by typescript.

--- alertify-js.d.ts
+++ alertify-js.d.ts
@@ -1,32 +1,161 @@
 // Type definitions for Alertify.js v1.0.11
 // Project: https://github.com/alertifyjs/alertify.js
-// Definitions by: Vlad Jerca <https://github.com/vladjerca>
+// Definitions by: Vlad Jerca <https://github.com/LabEG>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface IAlertify {
-    reset(): IAlertify;
-    alert(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
-    confirm(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
-    prompt(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
-    log(message: string, click?: Function): IAlertify;
-    theme(themeName: string): IAlertify;
-    success(message: string, click?: Function): IAlertify;
-    error(message: string, click?: Function): IAlertify;
-    cancelBtn(label: string): IAlertify;
-    okBtn(label: string): IAlertify;
-    delay(time: Number): IAlertify;
-    placeholder(str: string): IAlertify;
-    defaultValue(str: string): IAlertify;
-    maxLogItems(max: Number): IAlertify;
-    closeLogOnClick(bool: Boolean): IAlertify;
-    logPosition(position: string): IAlertify;
-    setLogTemplate(template: string): IAlertify;
-    clearLogs(): IAlertify;
-    parent(prt: HTMLElement): IAlertify;
+// https://alertifyjs.org/
+// WARNING: not https://alertifyjs.com/
+
+declare namespace Alertify {
+
+    interface IAlertifyStatic {
+
+        /**
+         *
+         */
+        version: string;
+
+        /**
+         *
+         * @param elem
+         */
+        parent(elem: HTMLElement): void;
+
+        /**
+         *
+         */
+        reset(): this;
+
+        /**
+         * Create an alert dialog box
+         * @param message   The message passed from the callee
+         * @param onOkay    Callback function
+         * @param onCancel  Callback function
+         * @return alertify (ie this)
+         * @since 0.0.1
+         */
+        alert(message: string, onOkay?: Function, onCancel?: Function): this;
+
+        /**
+         * Create a confirm dialog box
+         * @param message   The message passed from the callee
+         * @param onOkay    Callback function
+         * @param onCancel  Callback function
+         * @return alertify (ie this)
+         * @since 0.0.1
+         */
+        confirm(message: string, onOkay?: Function, onCancel?: Function): this;
+
+        /**
+         * Create a prompt dialog box
+         * @param message   The message passed from the callee
+         * @param onOkay    Callback function
+         * @param onCancel  Default value for prompt input
+         * @return alertify (ie this)
+         * @since 0.0.1
+         */
+        prompt(message: string, onOkay?: Function, onCancel?: Function): this;
+
+        /**
+         * Show a new log message box
+         * @param message   The message passed from the callee
+         * @param click      Callback function
+         * @return alertify (ie this)
+         * @since 0.0.1
+         */
+        log(message: string, click?: Function): this;
+
+        /**
+         *
+         * @param themeStr
+         */
+        theme(themeStr: string): this;
+
+        /**
+         * Shorthand for log messages
+         * @param message The message passed from the callee
+         * @param click      Callback function
+         * @return alertify (ie this)
+         * @since 0.0.1
+         */
+        success(message: string, click?: Function): this;
+
+        /**
+         * Shorthand for log messages
+         * @param message The message passed from the callee
+         * @param click      Callback function
+         * @return alertify (ie this)
+         * @since 0.0.1
+         */
+        error(message: string, click?: Function): this;
+
+        /**
+         *
+         * @param text
+         */
+        cancelBtn(text: string): this;
+
+        /**
+         *
+         * @param text
+         */
+        okBtn(text: string): this;
+
+        /**
+         *
+         * @param delay in ms
+         */
+        delay(delay: number): this;
+
+        /**
+         *
+         * @param text
+         */
+        placeholder(text: string): this;
+
+        /**
+         *
+         * @param text
+         */
+        defaultValue(text: string): this;
+
+        /**
+         *
+         * @param num
+         */
+        maxLogItems(num: number): this;
+
+        /**
+         *
+         * @param yes
+         */
+        closeLogOnClick(yes?: boolean): this;
+
+        /**
+         *
+         * @param text
+         */
+        logPosition(text?: string): this;
+
+        /**
+         *
+         * @param text
+         */
+        setLogTemplate(func: (input: string) => string): this;
+
+        /**
+         *
+         * @param text
+         */
+        clearLogs(text: string): this;
+
+    }
+
 }
 
-declare var alertify: IAlertify;
+declare const alertify: Alertify.IAlertifyStatic;
 
-declare module "alertify" {
-    export default alertify;
+declare module 'alertify.js' {
+    const alertify: Alertify.IAlertifyStatic;
+    export = alertify;
 }
